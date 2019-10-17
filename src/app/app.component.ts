8e210e5b89c86f82fb3e00fd7a174f6539a4099b
John Doe <johndoe@example.com>
2019-10-17 09:12:59 +0100

    added tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proto-angular-form-reset';
+
+  data = {
+    firstName: 'first',
+    lastName: 'last',
+    etc: null,
+  };
+
+  innerData = {
+    address: 'address',
+    postCode: 'postCode',
+  };
+
+  @ViewChild('form') private form: NgForm;
+  @ViewChild('firstName') private firstName: NgModel;
+
+  @ViewChild('form2') private form2: NgForm;
+  @ViewChild('innerForm') private innerForm: NgForm;
+
+  onSubmit(evt: Event) {
+    if (this.form.valid) {
+      evt.preventDefault();
+    } else {
+      console.log('submit');
+      console.log(this.form);
+      console.log(this.form.valid);
+    }
+
+    return false;
+  }
+
+  onReset(evt: Event) {
+    console.log('reset');
+    this.form.resetForm(this.data);
+  }
 }
